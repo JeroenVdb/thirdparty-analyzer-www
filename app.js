ed1cbe35526e1c3c2636f5afdc80947fd61c45a6
Jeroen Van den Berghe <Jeroen.Van.den.Berghe@persgroep.be>
2015-06-03 15:25:55 +0200

    Clean up JS

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 'use strict';
 
-var pp = require('party-pooper'),
+var config = require('./config'),
+	partyPooper = require('party-pooper'),
 	express = require('express'),
 	app = express(),
 	Busboy = require('busboy'),
-	inspect = require('util').inspect;
-
-var server;
+	inspect = require('util').inspect,
+	server;
 
 app.set('view engine', 'jade');
 
-server = app.listen(process.env.PORT || 3000, function() {
-	var host = server.address().address;
-	var port = server.address().port;
+server = app.listen(config.env, function() {
+	var host = server.address().address,
+	port = server.address().port;
 
 	console.log('Example app listening at http://%s:%s', host, port);
 });
@@ -39,15 +39,11 @@ app.post('/harpost', function(req, res) {
 		console.log('File [' + fieldname + ']: filename: ' + filename + ', encoding: ' + encoding + ', mimetype: ' + mimetype);
 		file.on('data', function(data) {
 			console.log('File [' + fieldname + '] got ' + data.length + ' bytes');
-			// console.log(data);
 			fullData += data;
 		});
 		file.on('end', function() {
 			console.log('File [' + fieldname + '] Finished');
-			// console.log(fullData);
-			// res.writeHead(303, { Connection: 'close', Location: '/' });
-			// console.log(fullData);
-			result = pp.run(JSON.parse(fullData), firstparty);
+			result = partyPooper.run(JSON.parse(fullData), firstparty);
 			res.render('result', {
 				'title': 'Third party pooper',
 				'message': 'Gimmy all your HAR!',
@@ -59,7 +55,6 @@ app.post('/harpost', function(req, res) {
 		console.log('Field [' + fieldname + ']: value: ' + inspect(val));
 		if (fieldname === 'firstparty') {
 			firstparty = val.split(/\r\n/);
-			console.log(firstparty);
 		}
 	});
 	busboy.on('finish', function() {
---
+++ config.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var config = {};
+
+/*eslint-disable */
+config.env = process.env.NODE_ENV || 3000;
+/*eslint-enable */
+
+module.exports = config;
